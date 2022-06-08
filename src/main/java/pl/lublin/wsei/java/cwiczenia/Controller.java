2a5366f0c37c9b61acece19a722572b7ac39fb82
Tomasz Stepien <tomasz.steppien@gmail.com>
2022-06-08 15:20:56 +0200

    (2b) Krok 5 - Wczytanie adresu i miniaturki w zależności od wybranego tytułu

--- src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,11 +1,17 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import java.io.File;
 
@@ -18,9 +24,31 @@ public class Controller {
     ObservableList<String> tytuly = FXCollections.observableArrayList();
     GusInfoGraphicList igList;
 
+    public ImageView imgMiniaturka;
+    public TextField txtAdresStrony;
+    public Button btnPokazInfografike;
+    public Button btnPrzejdzDoStrony;
+
     @FXML
     public void initialize() {
         fileChooser.getExtensionFilters().add(xmlFilter);
+        lstInfografiki.getSelectionModel().selectedIndexProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observableValue, Number old_val, Number new_val) {
+                        int index = new_val.intValue();
+                        if (index != -1) {
+                            txtAdresStrony.setText(igList.infografiki.get(index).adresStrony);
+                            Image image = new Image(igList.infografiki.get(index).adresMiniaturki);
+                            imgMiniaturka.setImage(image);
+                        }
+                        else {
+                            txtAdresStrony.setText("");
+                            imgMiniaturka.setImage(null);
+                        }
+                    }
+                }
+        );
     }
 
     public void btnOpenFileAction(ActionEvent actionEvent) {
