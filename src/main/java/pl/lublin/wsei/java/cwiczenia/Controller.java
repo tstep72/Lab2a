a309295e9f574b98768c331b872120f379ee0fa2
Tomasz Stepien <tomasz.steppien@gmail.com>
2022-06-08 14:35:25 +0200

    (2b) Kroki 1-3c - Budowa AnchorPane

--- src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -3,7 +3,7 @@ package pl.lublin.wsei.java.cwiczenia;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
